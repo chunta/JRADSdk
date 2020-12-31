da34f982bb009da04f04bbd83772b59c1a9fc9f2
chunta <imchuntachen@gmail.com>
2020-12-31 11:06:47 +0800

    init

---
+++ JRADSdk.xcframework/ios-arm64_i386_x86_64-simulator/JRADSdk.framework/Headers/JRADSdk.h
@@ -0,0 +1,20 @@
+//
+//  JRADSdk.h
+//  JRADSdk
+//
+//  Created by chunta on 2020/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JRADSdk.
+FOUNDATION_EXPORT double JRADSdkVersionNumber;
+
+//! Project version string for JRADSdk.
+FOUNDATION_EXPORT const unsigned char JRADSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRADSdk/PublicHeader.h>
+
+#import <JRADSdk/AIDPage.h>
+#import <JRADSdk/Legacy.h>
+
